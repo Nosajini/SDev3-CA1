984fbcca452caa023f2db619aaa33c0fef44a09d
Grahame <x00179536@mytudublin.ie>
2021-11-03 12:48:24 +0000

    Linked Static, Navbar in base.html, linked home and login page

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import homePageView
+from .views import homePageView, loginPageView
 
 urlpatterns = [
-    path('', homePageView.as_view(), name='home')
+    path('', homePageView.as_view(), name='home'),
+    path('login/', loginPageView.as_view(), name='login'),
 ]
 
--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,6 @@ from django.views.generic import TemplateView
 
 class homePageView(TemplateView):
     template_name = 'home.html'
+
+class loginPageView(TemplateView):
+    template_name = 'login.html'
