513ca656ff29237e0a02bb38a2ef912634d86c0f
Jason Hallinan <X00176047@mytudublin.ie>
2021-11-05 16:34:34 +0000

    Made custom signup form, made a custom user model

---
+++ accounts/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    model = CustomUser
+    fields = ('username', 'email', 'password1', 'password2',)
+        
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
