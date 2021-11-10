a683dc5c8238f0eccc2889817c44ca99fc340e3d
Grahame <x00179536@mytudublin.ie>
2021-11-10 12:22:35 +0000

    Added posts tab containing post_list + working images

--- CA1_Project/settings.py
+++ CA1_Project/settings.py
@@ -126,8 +126,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
 MEDIA_URL = '/media/'
-MEDIA_rOOT = str(BASE_DIR.joinpath('media'))
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,25 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
 
 # Create your views here.
 
 class PostListView(ListView):
     model = Post
-    template_name = 'home.html'
+    template_name = 'post_list.html'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ('title', 'body', 'image', 'author')
+    template_name = 'post_new.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
