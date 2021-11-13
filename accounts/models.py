edf8ad3b80dfc48748958b013a4dfa915e9daf1b
Jason Hallinan <X00176047@mytudublin.ie>
2021-11-13 21:25:07 +0000

    Profiles are now automatically created along with users. Author names now link to their respective profiles.

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth import get_user_model
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
 class CustomUser(AbstractUser):
@@ -8,6 +10,15 @@ class CustomUser(AbstractUser):
 
 class Profile(models.Model):
     user = models.OneToOneField(get_user_model(), null=True, on_delete=models.CASCADE,)
+
+    @receiver(post_save, sender=CustomUser)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=CustomUser)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
 
     def __str__(self):
         return str(self.user)
