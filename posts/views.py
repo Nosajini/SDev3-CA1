63eed7a62a4c16f1be57bfd5b31e228d4849769e
Jason Hallinan <X00176047@mytudublin.ie>
2021-11-14 16:28:44 +0000

    Added delete and edit links in post detail, where the user is the post's author. Edit and Delete pages ensure that only the post's author is allowed to edit or delete the respective post.

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,4 @@
-from django.db import models
-from django.db.models import fields
 from django.views.generic import ListView, DetailView
-from django.views.generic.base import TemplateView
 from .models import Post
 from .forms import ImageForm
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
@@ -23,15 +20,24 @@ class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
-class PostUpdateView(UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['body']
     template_name = 'post_edit.html'
+    
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+    
 
-class PostDeleteView(DeleteView):
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy('post_list')
+    
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
