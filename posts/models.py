064fbbc4c9521d6776c1ee3802b307e8620ff924
Jason Hallinan <X00176047@mytudublin.ie>
2021-11-14 20:34:36 +0000

    Added comment functionality. Comment images are optional, comments lacking images will not crash the page.

--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,7 @@ import uuid
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.db.models.expressions import OrderBy
 from django.urls import reverse
 
 # Create your models here.
@@ -27,3 +28,27 @@ class Post(models.Model):
     
     class Meta():
         ordering = ('date',)
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete = models.CASCADE,
+        related_name="comments"
+    )
+    image = models.ImageField(blank=True, upload_to='post/comment/')
+    comment = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now_add=True)
+    author= models.ForeignKey(
+        get_user_model(),
+        on_delete = models.CASCADE
+    )
+
+    def has_image(self):
+        return self.image
+    
+    def __str__(self):
+        return self.comment
+
+    class Meta():
+        ordering = ('date',)
+
